cb3b3382ae284f22fc8180688c31320a5780f547
Baptiste Covolato <b.covolato@gmail.com>
2013-07-12 05:09:30 +0200

    [GDESUGAR] Add desugaring of Modules

--- src/desugar/global-desugar.cc
+++ src/desugar/global-desugar.cc
@@ -1,6 +1,31 @@
 #include <desugar/global-desugar.hh>
+#include <ast/all.hh>
 
 namespace desugar
 {
+    GlobalDesugar::GlobalDesugar()
+        : global_space_(true)
+        , init_fun_(nullptr)
+        , init_body_(nullptr)
+    {}
 
+    GlobalDesugar::~GlobalDesugar()
+    {}
+
+    void GlobalDesugar::operator()(ast::ModuleStmt& ast)
+    {
+        init_body_ = new ast::StmtList(yy::location());
+
+        init_fun_ = new ast::FunctionDec(yy::location(), "__init", nullptr,
+                                         init_body_);
+
+        ast::AstList* content = clone(ast.content_get());
+
+        ast::ModuleStmt* s = new ast::ModuleStmt(ast.location_get(),
+                                                 ast.name_get(), content);
+
+        s->content_get()->push_back(init_fun_);
+
+        cloned_ast_ = s;
+    }
 } // namespace desugar
--- src/desugar/global-desugar.hh
+++ src/desugar/global-desugar.hh
@@ -1,3 +1,7 @@
+// The goal of this desugar visitor is to extract all the global components
+// and put it in an __init function that will be called on every namespace
+// later. Futhermore it will also flaged variables that are global. Then, later
+// we will be able to declared them as global in the namespace module
 #ifndef DESUGAR_GLOBAL_DESUGAR_HH
 # define DESUGAR_GLOBAL_DESUGAR_HH
 
@@ -7,7 +11,18 @@ namespace desugar
 {
     class GlobalDesugar : public cloner::AstCloner
     {
+        public:
+            using cloner::AstCloner::operator();
 
+            GlobalDesugar();
+            virtual ~GlobalDesugar();
+
+            virtual void operator()(ast::ModuleStmt& ast);
+
+        protected:
+            bool global_space_;
+            ast::FunctionDec* init_fun_;
+            ast::StmtList* init_body_;
     };
 } // namespace desugar
 
