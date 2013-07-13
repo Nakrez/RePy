ed11d39f669416529a1d25f49396c492b0c0f60f
Baptiste Covolato <b.covolato@gmail.com>
2013-07-13 23:47:02 +0200

    [GENERATOR] Add generation of ModuleStmt C++

--- src/cpp/generator.cc
+++ src/cpp/generator.cc
@@ -9,4 +9,15 @@ namespace cpp
 
     Generator::~Generator()
     {}
+
+    void Generator::operator()(ast::ModuleStmt& ast)
+    {
+        o_ << "namespace __" << ast.name_get() << "__" << misc::iendl;
+        o_ << "{" << misc::indentendl;
+
+        ast.content_get()->accept(*this);
+
+        o_ << misc::dedentendl;
+        o_ << "}" << misc::iendl;
+    }
 } // namespace cpp
--- src/cpp/generator.hh
+++ src/cpp/generator.hh
@@ -1,6 +1,8 @@
 #ifndef CPP_GENERATOR_HH
 # define CPP_GENERATOR_HH
 
+# include <misc/indent.hh>
+
 # include <ast/default-visitor.hh>
 
 namespace cpp
@@ -8,8 +10,12 @@ namespace cpp
     class Generator : public ast::DefaultVisitor
     {
         public:
+            using ast::DefaultVisitor::operator();
+
             Generator(std::ostream& o);
             virtual ~Generator();
+
+            virtual void operator()(ast::ModuleStmt& ast);
 
         protected:
             std::ostream& o_;
