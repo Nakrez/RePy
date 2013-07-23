b29ec3e09e7094a66803788c0ed0df72d719bfc3
Baptiste Covolato <b.covolato@gmail.com>
2013-07-23 03:29:19 +0200

    [RUNTIME] Add __add__ operation

--- lib/repy/repy.hxx
+++ lib/repy/repy.hxx
@@ -6,6 +6,18 @@
 namespace repy
 {
     template<class T>
+    T __add__(T a, T b)
+    {
+        return a.__add__(b);
+    }
+
+    template <>
+    __repy_int __add__(__repy_int a, __repy_int b)
+    {
+        return a + b;
+    }
+
+    template<class T>
     T __mul__(T a, T b)
     {
         return a.__mul__(b);
